f09891c1b1f359e5cf0971b152ee7a0351bd96aa
zhangyiyong <287069934@qq.com>
2018-08-29 17:29:11 +0800

    fix: 修复组件展示页面bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import DxSelectPage from './dxselectpage.vue'
-import DxRadioPage from './dxradiopage.vue'
-import DxTablePage from './dxtablepage.vue'
-import DxButtonPage from './dxbuttonpage.vue'
-import DxNavPage from './dxnavpage.vue'
-import DxInputPage from './dxInputpage.vue'
-import DxPaginationPage from './dxpaginationpage.vue'
+import DxSelectPage from './DxSelectPage.vue'
+import DxRadioPage from './DxRadioPage.vue'
+import DxTablePage from './DxTablePage.vue'
+import DxButtonPage from './DxButtonPage.vue'
+import DxNavPage from './DxNavPage.vue'
+import DxInputPage from './DxInputPage.vue'
+import DxPaginationPage from './DxPaginationPage.vue'
 import DxFormPage from './DxFormPage.vue'
 import DxCarouselPage from './DxCarouselPage.vue'
 import DxNotifyPage from './DxNotifyPage.vue'
@@ -19,9 +19,9 @@ const pages = [
     { path: 'dxnavpage', title: 'nav导航栏', component: DxNavPage },
     { path: 'dxInputpage', title: 'input输入框', component: DxInputPage },
     { path: 'dxpaginationpage', title: 'pagination分页', component: DxPaginationPage },
-    { path: 'DxCarouselPage', title: 'carousel轮播组件', component: DxCarouselPage },
-    { path: 'DxNotifyPage', title: 'notify消息通知组件', component: DxNotifyPage },
-    { path: 'DxProgessPage', title: 'progress进度条', component: DxProgessPage },
-    { path: 'DxUploadPage', title: 'upload文件上传', component: DxUploadPage }
+    { path: 'dxcarouselpage', title: 'carousel轮播组件', component: DxCarouselPage },
+    { path: 'dxnotifypage', title: 'notify消息通知组件', component: DxNotifyPage },
+    { path: 'dxprogesspage', title: 'progress进度条', component: DxProgessPage },
+    { path: 'dxuploadpage', title: 'upload文件上传', component: DxUploadPage }
 ]
 export default pages
--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,14 @@
 import VueRouter from 'vue-router';
-import Home from '../pages/home.vue';
+import Home from '../pages/Home.vue';
 import pages from '../pages/index.js';
 // 导入所有的页面
 const children =  new Array(pages.length);
 pages.forEach(function(page){
-	children.push({path:page.path,component:page.component})
+	children.push({path: page.path, component: page.component})
 })
 
 const routes = [
-    { path:'/', component: Home, children:children}
+    { path:'/', component: Home, children: children}
 ]
 export default new VueRouter({
 	routes
